cd7fbb2bdc83894dfd7169f6b497765295a970fd
Nikolay Gubankov <nikgub@cock.li>
2024-05-21 01:02:55 +0300

    Added a config option to display available moves at render

--- lab_7/config.hpp
+++ lab_7/config.hpp
@@ -2,3 +2,8 @@
  * Defines if board should be displayed in green/red instead of white/black
  */
 //#define HIGH_CONTRAST
+
+/*
+ * Defines if danger should be immedeatly rendered
+ */
+#define RENDER_DANGER
--- lab_7/cpps/renderer.cpp
+++ lab_7/cpps/renderer.cpp
@@ -4,11 +4,35 @@ using namespace chess::render;
 
 void renderer::collect (board Board)
 {
+#if defined (RENDER_DANGER)
+#define DANGER '.' 
     for (loc_t i = 0; i < 64; i++)
     {
         if (Board.data()[i].type()) ToRender[i] = Board.data()[i].type()->display();
         else ToRender[i] = ' ';
     }
+    std::array<loc_t, 64> danger;
+    loc_t c = 0;
+    for (loc_t i = 0; i < 64; i++)
+    {
+        if (ToRender[i] != ' ')
+        {
+            piece_type * tptr = Board.data()[i].type();
+            if (!tptr) continue;
+            for (loc_t j = 0; j < 64; j++)
+            {
+                if (i == j) continue;
+                if (tptr->move(Board.data()[i], Board.data()[j])) ToRender[j] = DANGER;
+            }
+        }
+    }
+#else
+    for (loc_t i = 0; i < 64; i++)
+    {
+        if (Board.data()[i].type()) ToRender[i] = Board.data()[i].type()->display();
+        else ToRender[i] = ' ';
+    }
+#endif
 }
 
 void renderer::draw ()
@@ -22,10 +46,18 @@ void renderer::draw ()
 #if defined (HIGH_CONTRAST)
             const char * dec = (r == ' ')
                 ? ((x + y + 1) % 2) ? "\033[0;101m" : "\033[0;102m"
+#if defined (RENDER_DANGER)
+                : (r == DANGER)
+                ? "\033[0;104m"
+#endif
                 : ((x + y + 1) % 2) ? "\033[0;91m" : "\033[0;92m";
 #else
             const char * dec = (r == ' ')
                 ? ((x + y + 1) % 2) ? "\033[0;100m" : "\033[0;107m"
+#if defined (RENDER_DANGER)
+                : (r == DANGER)
+                ? "\033[0;104m"
+#endif
                 : ((x + y + 1) % 2) ? "\033[0;90m" : "\033[0;97m";
 #endif
             std::cout << dec << r << "\033[0m" << ' ';
